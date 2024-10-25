fe1b8a68eb434c19dd19f3a199b791a9ffff212e
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2024-10-25 18:27:34 -0400

    moved to packages

--- src/test/kotlin/data/FileTest.kt
+++ src/test/kotlin/data/FileTest.kt
@@ -2,16 +2,11 @@
 
 package data
 
-import data.repo.Repo
-import data.repo.RepoFile
-import data.storage.Location
-import data.storage.StoredFileVersion
+                                                         import data.repo.Repo
+import data.storage.readFolder
 import java.io.File
-import java.io.IOException
-import java.security.MessageDigest
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
-import kotlin.io.path.fileSize
 import kotlin.test.Test
 
 fun Repo.dumpToConsole() {
@@ -23,33 +18,12 @@ fun Repo.dumpToConsole() {
     println("END DUMPING REPO")
 }
 
-fun readFolder(path: File): Repo {
-    val md5 = MessageDigest.getInstance("md5")
-    val allFiles = path.walk().filter { it.isFile }.mapNotNull {
-        val digest = try {
-            md5.digest(it.readBytes())
-        } catch (e: IOException) {
-            return@mapNotNull null
-        }
-
-        StoredFileVersion(
-            size = it.toPath().fileSize(),
-            hash = digest,
-            path = it.path,
-            location = Location.LocalFilesystem
-        )
-    }.toList()
-    return Repo(
-        files = allFiles.map { RepoFile(logicalPath = it.path, hash = it.hash) },
-        storage = allFiles
-    )
-}
 
 class FileTest {
 
     @Test
     fun `traverse project folder`() {
-        val repo: Repo = readFolder(File("./"))
+        val repo: Repo = readFolder(File("./.experiments/data"))
 
         repo.dumpToConsole()
     }
--- src/test/kotlin/data/storage/File.kt
+++ src/test/kotlin/data/storage/File.kt
@@ -1,8 +1,41 @@
 package data.storage
 
+import data.repo.Repo
+import data.repo.RepoFile
+import java.io.File
+import java.io.IOException
+import java.security.MessageDigest
+import kotlin.io.path.fileSize
+
 interface Location {
     object LocalFilesystem: Location
 }
 
-class StoredFileVersion(val path: String, val location: Location, val hash: ByteArray, val size: Long)
+class StoredFileVersion(val path: String, val location: Location, val hash: ByteArray, val size: Long) {
+    init {
+        check(!File(path).isAbsolute) {"Path $path is not absolute"}
+    }
+}
 
+
+fun readFolder(rootFolder: File): Repo {
+    val md5 = MessageDigest.getInstance("md5")
+    val allFiles = rootFolder.walk().filter { it.isFile }.mapNotNull {
+        val digest = try {
+            md5.digest(it.readBytes())
+        } catch (e: IOException) {
+            return@mapNotNull null
+        }
+
+        StoredFileVersion(
+            size = it.toPath().fileSize(),
+            hash = digest,
+            path = it.relativeTo(rootFolder).path,
+            location = Location.LocalFilesystem
+        )
+    }.toList()
+    return Repo(
+        files = allFiles.map { RepoFile(logicalPath = it.path, hash = it.hash) },
+        storage = allFiles
+    )
+}
