9d5484599a0e4a91e53d1464fb8491b92127acfd
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2024-10-27 21:06:39 -0400

    deprecating unnecessary classes
    can be subsumed by sqlite tables

--- src/main/kotlin/data/storage/Block.kt
+++ src/main/kotlin/data/storage/Block.kt
@@ -1,11 +1,13 @@
 package data.storage
 
+@Deprecated("unneeded, really")
 interface Block {
     val size: Int
     val hash: Hash
     val data: ByteArray
 }
 
+@Deprecated("use data.repo.sql.datablocks")
 class FileReference(val file: FileSystem.File, val fileHash: Hash, val from: Long, val to: Long) {
     val size: Long = to - from
     val data: ByteArray
@@ -14,6 +16,7 @@ class FileReference(val file: FileSystem.File, val fileHash: Hash, val from: Lon
     override fun toString(): String = "{$file from $from to $to}"
 }
 
+@Deprecated("use data.repo.sql.datablocks")
 class LiveBlock(override val size: Int, val files: List<FileReference>) : Block {
     override val data: ByteArray
         get() = files.map { it.data }.reduce { a, b -> a.plus(b) } + ByteArray((size - files.sumOf { it.size }).toInt())
@@ -21,9 +24,11 @@ class LiveBlock(override val size: Int, val files: List<FileReference>) : Block 
     override val hash: Hash by lazy { Hash.digest(data) }
 }
 
+@Deprecated("use data.repo.sql.parity")
 open class MemoryBlock(final override val data: ByteArray) : Block {
     override val hash: Hash by lazy { Hash.digest(data) }
     override val size: Int = data.size
 }
 
+@Deprecated("use data.repo.sql.parity")
 class ParityBlock(data: ByteArray): MemoryBlock(data)
