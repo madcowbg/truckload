894a42ceac632ac3315e91d1463090a6725d7e28
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2024-10-29 13:55:30 -0400

    expanded logic, not yet done

--- src/test/kotlin/data/CreateCollectionTest.kt
+++ src/test/kotlin/data/CreateCollectionTest.kt
@@ -2,15 +2,20 @@ package data
 
 import data.repo.sql.StoredRepo
 import data.repo.sql.catalogue.FileVersions
+import data.repo.sql.datablocks.DataBlocks
 import data.repo.sql.datablocks.FileDataBlockMappings
+import data.repo.sql.datablocks.FileRefs
 import data.repo.sql.listOfIssues
+import data.repo.sql.parity.ParityBlocks
 import data.repo.sql.parity.ParityDataBlockMappings
+import data.repo.sql.parity.ParitySets
 import data.repo.sql.storagemedia.FileLocations
 import data.repo.sql.storagemedia.ParityLocations
 import data.repo.sql.storagemedia.StorageMedias
 import data.storage.DeviceFileSystem
 import data.storage.ReadonlyFileSystem
 import data.storage.WritableDeviceFileSystem
+import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
@@ -137,11 +142,99 @@ fun <StorageMedia> restoreCollection(
 
             println("Backup file $restorePath is not available, restoring via parity checks.")
 
-            val necessaryDataBlocks = FileDataBlockMappings.select(FileDataBlockMappings.dataBlockHash)
+            val necessaryDataBlocks: List<String> = FileDataBlockMappings.select(FileDataBlockMappings.dataBlockHash)
                 .where { FileDataBlockMappings.fileHash eq fileHash }
                 .map { it[FileDataBlockMappings.dataBlockHash] }
 
             println("Will restore file with ${necessaryDataBlocks.size} data blocks.")
+
+            data class ParitySetToRestore(val paritySetId: String)
+
+            // parity blocks
+            val paritySetIds: List<ParitySetToRestore> =
+                (FileDataBlockMappings innerJoin DataBlocks innerJoin ParityDataBlockMappings innerJoin ParitySets)
+                    .selectAll() // fixme optimise
+                    .where { FileDataBlockMappings.fileHash eq fileHash }
+                    .map { ParitySetToRestore(it[ParitySets.hash]) }
+
+            data class ParityBlockToRestore(
+                val paritySetId: String,
+                val parityPHash: String,
+                val parityFile: ReadonlyFileSystem.File?
+            )
+
+            val parityRestoreBlocks: List<ParityBlockToRestore> =
+                (ParityDataBlockMappings innerJoin ParitySets.join(
+                    ParityBlocks,
+                    JoinType.INNER,
+                    onColumn = ParityBlocks.hash,
+                    otherColumn = ParitySets.parityPHash
+                ) innerJoin ParityLocations innerJoin StorageMedias)
+
+                    .selectAll() // fixme optimise
+                    .where { ParityDataBlockMappings.paritySetId.inList(paritySetIds.map { it.paritySetId }) }
+                    .map {
+                        val storageDevice = storageDeviceByGuid[it[StorageMedias.guid]]
+                        val parityLocationFilePath =
+                            storageDevice?.resolve(".repo/parity_blocks/${it[ParityLocations.hash]}.parity")
+                        ParityBlockToRestore(
+                            it[ParityDataBlockMappings.paritySetId],
+                            it[ParitySets.parityPHash],
+                            parityLocationFilePath
+                        )
+                    }
+
+            parityRestoreBlocks.forEach { println(it) }
+
+            data class LiveToRestore(
+                val paritySetId: String,
+                val fileHash: String,
+                val fileOffset: Long,
+                val chunkSize: Long,
+                val dataBlockHash: String,
+                val blockOffset: Long,
+                val file: ReadonlyFileSystem.File?,
+            )
+
+            val dataRestoreBlocks: List<LiveToRestore> =
+                (ParityDataBlockMappings innerJoin DataBlocks innerJoin FileDataBlockMappings innerJoin FileRefs innerJoin FileLocations innerJoin StorageMedias)
+                    .selectAll() // fixme optimise
+                    .where { ParityDataBlockMappings.paritySetId.inList(paritySetIds.map { it.paritySetId }) }
+                    .map {
+                        val storageDevice = storageDeviceByGuid[it[StorageMedias.guid]]
+                        val storageLocationFilePath = it[FileLocations.path]
+                        LiveToRestore(
+                            it[ParityDataBlockMappings.paritySetId],
+                            it[FileDataBlockMappings.fileHash],
+                            it[FileDataBlockMappings.fileOffset],
+                            it[FileDataBlockMappings.chunkSize],
+                            it[FileDataBlockMappings.dataBlockHash],
+                            it[FileDataBlockMappings.blockOffset],
+                            storageDevice?.resolve(storageLocationFilePath),
+                        )
+                    }
+
+            dataRestoreBlocks.forEach { println(it) }
+
+            val parityRestoreBlocksByParitySet = parityRestoreBlocks.groupBy { it.paritySetId }
+            val dataRestoreBlocksByParitySet = dataRestoreBlocks.groupBy { it.paritySetId }
+
+            dataRestoreBlocksByParitySet.forEach { (paritySetId, liveBlocks) ->
+                val parityBlocks = parityRestoreBlocksByParitySet[paritySetId] // todo what if null??
+
+
+
+                println(paritySetId + " " + liveBlocks.size)
+
+            }
+            necessaryDataBlocks.map { blockHash ->
+                dataRestoreBlocksByParitySet
+
+            }
+//
+
+//                .join(ParityDataBlockMappings, JoinType.INNER, onColumn = DataBlocks.hash, otherColumn = Parity)
+//                .selectAll()
 
             // fetch parity sets
             // fetch necessary live blocks (in memory blocks, for now)
